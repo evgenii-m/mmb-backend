b682ba1d5567ae21463f4436ce1b9810094289ef
Evgenii M <mez.e.s@yandex.ru>
2020-10-31 23:53:35 +0300

    Implemented get authorization link method.

--- src/main/java/ru/pushkin/mma/api/output/AuthorizationController.java
+++ src/main/java/ru/pushkin/mma/api/output/AuthorizationController.java
@@ -1,5 +1,19 @@
 package ru.pushkin.mma.api.output;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.pushkin.mma.deezer.DeezerApiService;
 
+@RestController
 public class AuthorizationController {
+
+    @Autowired
+    private DeezerApiService deezerApiService;
+
+    @GetMapping("/auth/deezer")
+    public ResponseEntity<String> getDeezerAuthPageUrl() {
+        return ResponseEntity.ok(deezerApiService.getUserAuthorizationPageUrl());
+    }
 }
